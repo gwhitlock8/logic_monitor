e4d348f64e36addf772d46cf72214ba21bb34a4b
Gavin Whitlock <gavinwhitlock@atx-gwhitlock.lan>
2020-11-13 09:46:49 -0600

    updated "for in" loop to have "try catch"

--- paypal_portals.groovy
+++ paypal_portals.groovy
@@ -4,13 +4,10 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import org.apache.commons.codec.binary.Hex;import com.santaba.agent.groovyapi.http.*;
-import groovy.json.JsonSlurper;
-	
-def hostName = java.net.URLEncoder.encode(hostProps.get("system.displayname"));
-
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import org.apache.commons.codec.binary.Hex;import com.santaba.agent.groovyapi.http.*
+import groovy.json.JsonSlurper
 //Account Info
 Map credentials = [
     "paypal": [
@@ -35,51 +32,44 @@ Map data = [
     "corp":[],
     "lb":[]
 ]
-
-
-resourcePath = "/metrics/usage";
-
+resourcePath = "/metrics/usage"
 //get current time
-epoch = System.currentTimeMillis();
-
+epoch = System.currentTimeMillis()
 //calculate signature
-requestVars = "GET" + epoch + resourcePath;
-
+requestVars = "GET" + epoch + resourcePath
 //define API endpoint path
-
-hmac = Mac.getInstance("HmacSHA256");
-
-CloseableHttpClient httpclient = HttpClients.createDefault();
-
+hmac = Mac.getInstance("HmacSHA256")
+CloseableHttpClient httpclient = HttpClients.createDefault()
 if (credentials.paypal.id && credentials.corp.id && credentials.lb.id) {
     //loop through credentials to build URL for each respective portal
     for(p in credentials) {
-        secret = new SecretKeySpec((p.value.key).getBytes(), "HmacSHA256");
-        hmac.init(secret);
-        hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()));
-        signature = hmac_signed.bytes.encodeBase64();
-
-        url = "https://" + p.value.account + ".logicmonitor.com" + "/santaba/rest" + resourcePath;
-
+        secret = new SecretKeySpec((p.value.key).getBytes(), "HmacSHA256")
+        hmac.init(secret)
+        hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()))
+        signature = hmac_signed.bytes.encodeBase64()
+        url = "https://" + p.value.account + ".logicmonitor.com" + "/santaba/rest" + resourcePath
         //HTTP Get
-        httpGet = new HttpGet(url);
-        httpGet.addHeader("Authorization","LMv1" + "$p.value.id" + ":" + signature + ":" + epoch);
-        response = httpclient.execute(httpGet);
-        responseBody = EntityUtils.toString(response.getEntity());
-        code = response.getStatusLine().getStatusCode();
-
+        httpGet = new HttpGet(url)
+        httpGet.addHeader("Authorization","LMv1" + "$p.value.id" + ":" + signature + ":" + epoch)
+        httpGet.addHeader("X-Version","2")
+        try {response = httpclient.execute(httpGet)}
+        catch (Exception e){println(e);return 1}
+        responseBody = EntityUtils.toString(response.getEntity())
+        code = response.getStatusLine().getStatusCode()
+        println(responseBody)
         //user groovy slurper
-        json_slurper = new JsonSlurper();
-        response_obj = json_slurper.parseText(responseBody);
-        data[p.key] = response_obj
-
+        if (code == "200"){
+          json_slurper = new JsonSlurper()
+          response_obj = json_slurper.parseText(responseBody)
+          data[p.key] = response_obj
+        } else {
+          return code
+        }
     }
 } else {
     println("Device is not configured with the necessary portal credentials to proceed with API queries.\n" +
             "Exiting Program...")
 }
 return 0 //Exit Successfully
-
 data.each() {k, v -> println("$k=$v")}
-httpclient.close();
-
+httpclient.close()
