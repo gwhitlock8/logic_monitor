653c4021fd6f1184bf85b44cd471debf0f07fa25
Gavin Whitlock <gavinwhitlock@atx-gwhitlock.lan>
2020-11-24 13:56:07 -0600

    fixed the auth error...do I know what I did to fix it...absolutely not

--- paypal_portals.groovy
+++ paypal_portals.groovy
@@ -9,6 +9,7 @@ import javax.crypto.spec.SecretKeySpec
 import org.apache.commons.codec.binary.Hex;import com.santaba.agent.groovyapi.http.*
 import groovy.json.JsonSlurper
 //Account Info
+
 Map credentials = [
     "paypal": [
         "id": hostProps.get("lmaccess.id"),
@@ -32,50 +33,59 @@ Map data = [
     "corp":[],
     "lb":[]
 ]
+
 def resourcePath = "/metrics/usage"
-//get current time
-//calculate signature
+
+
 //define API endpoint path
-if (credentials.paypal.id && credentials.corp.id && credentials.lb.id) {
-    //loop through credentials to build URL for each respective portal
-    credentials.each { p ->
-        url = "https://" + p.value.account + ".logicmonitor.com" + "/santaba/rest" + resourcePath
 
-        epoch = System.currentTimeMillis()
-        requestVars = "GET" + epoch + resourcePath
-        hmac = Mac.getInstance("HmacSHA256")
-        secret = new SecretKeySpec((p.value.key).getBytes(), "HmacSHA256")
-        hmac.init(secret)
-        hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()))
-        signature = hmac_signed.bytes.encodeBase64()
+credentials.each{ entry -> 
+    
+    url = "https://" + entry.value.account + ".logicmonitor.com" + "/santaba/rest" + resourcePath
 
-        //HTTP Get
-        CloseableHttpClient httpclient = HttpClients.createDefault()
-        httpGet = new HttpGet(url)
-        httpGet.addHeader("Authorization","LMv1" + "$p.value.id" + ":" + signature + ":" + epoch)
-        httpGet.addHeader("X-Version","2")
-        try {
-            response = httpclient.execute(httpGet)
-            responseBody = EntityUtils.toString(response.getEntity())
-            code = response.getStatusLine().getStatusCode()
-            println(responseBody)
-        } catch (Exception e){
-            println(e, "ERROR????");return 1
-        }
-        
-        //user groovy slurper
-        if (code == "200"){
-          json_slurper = new JsonSlurper()
-          response_obj = json_slurper.parseText(responseBody)
-          data[p.key] = response_obj
-        } else {
-          return code
-        }
-        httpclient.close()
+    //get current time
+    epoch = System.currentTimeMillis()
+
+    //calculate signature
+    requestVars = "GET" + epoch + resourcePath
+    
+    hmac = Mac.getInstance("HmacSHA256");
+    secret = new SecretKeySpec((entry.value.key).getBytes(), "HmacSHA256");
+    hmac.init(secret);
+    hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()));
+    signature = hmac_signed.bytes.encodeBase64();
+
+
+    // HTTP Get
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    httpGet = new HttpGet(url);
+    httpGet.addHeader("Authorization" , "LMv1 " + entry.value.id + ":" + signature + ":" + epoch);
+    httpGet.addHeader("X-Version","2")
+    try {
+    response = httpclient.execute(httpGet);
+    responseBody = EntityUtils.toString(response.getEntity());
+    code = response.getStatusLine().getStatusCode();
+    
+
+    } catch (Exception err) {
+    println("ERROR: Script failed while attempting to query $url API endpoint...\n${err?.message}")
     }
-} else {
-    println("Device is not configured with the necessary portal credentials to proceed with API queries.\n" +
-            "Exiting Program...")
+    response = httpclient.execute(httpGet);
+    responseBody = EntityUtils.toString(response.getEntity());
+    code = response.getStatusLine().getStatusCode();
+            
+    json_slurper = new JsonSlurper();
+    response_obj = json_slurper.parseText(responseBody);
+    println response_obj
+
+    httpclient.close()
+
+    
 }
+
+
 return 0 //Exit Successfully
-data.each() {k, v -> println("$k=$v")}
+
+
+
+
